1683399196d809c11ed2ff8ef678d63695e868c8
Mojtaba zamani <memojtabazamani@gmail.com>
2026-05-22 00:02:16 +0330

    app.js

---
+++ git-undo-practice/app.js
@@ -0,0 +1,12 @@
+// حالت اولیه فایل
+function greeting(name) {
+    console.log("Hello");
+}
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+function showMessage(msg) {
+    alert(msg);
+}
